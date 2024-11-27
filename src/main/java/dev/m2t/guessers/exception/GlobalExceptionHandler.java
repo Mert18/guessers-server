35e565ca0d1eb683a777b51bd1e8573c2acd04b2
Abdurrahman Derda Karaaslan <derdakaraaslan02@gmail.com>
2024-11-27 23:54:22 +0300

    all UsernameNotFoundExceptions changed to ResourceNotFoundException

--- src/main/java/dev/m2t/guessers/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/m2t/guessers/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package dev.m2t.guessers.exception;
 import dev.m2t.guessers.dto.BaseResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
